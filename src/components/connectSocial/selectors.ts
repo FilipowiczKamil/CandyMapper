228528d25f50fcd75cca24ef80b0adf6d2d475da
filipowicz.kamil <fkamil1609@gmail.com>
2025-07-29 20:25:13 +0200

    connectSocials component

---
+++ src/components/connectSocial/selectors.ts
@@ -0,0 +1,10 @@
+export const connectSocialSelectors = {
+  componentWrapper: '[id="0f1719fa-57fa-4d21-89f8-c63672863a2a"]',
+  socialLinksWrapper: '[data-aid="SOCIAL_SOCIAL_LINKS"]',
+  facebookLink: '[data-aid="SOCIAL_FACEBOOK_LINK"]',
+  instagramLink: '[data-aid="SOCIAL_INSTAGRAM_LINK"]',
+  linkedinLink: '[data-aid="SOCIAL_LINKEDIN_LINK"]',
+  pinterestLink: '[data-aid="SOCIAL_PINTEREST_LINK"]',
+  xLink: '[data-aid="SOCIAL_TWITTER_LINK"]',
+  youtubeLink: '[data-aid="SOCIAL_YOUTUBE_LINK"]',
+};
